aac557248d0513ea428e613edd9f8a8f9cfe2f25
Armando Nuñez_404 <114707950+armandonum@users.noreply.github.com>
2022-12-13 14:33:09 -1100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace Lista_Doblemente_Enlazada_Circular_N_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Insertar = new System.Windows.Forms.Button();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.Primero = new System.Windows.Forms.Button();
+            this.Anterior = new System.Windows.Forms.Button();
+            this.Siguiente = new System.Windows.Forms.Button();
+            this.Ultimo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Insertar
+            // 
+            this.Insertar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Insertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Insertar.Location = new System.Drawing.Point(-1, 157);
+            this.Insertar.Margin = new System.Windows.Forms.Padding(2);
+            this.Insertar.Name = "Insertar";
+            this.Insertar.Size = new System.Drawing.Size(107, 39);
+            this.Insertar.TabIndex = 0;
+            this.Insertar.Text = "Insertar Detras";
+            this.Insertar.UseVisualStyleBackColor = false;
+            this.Insertar.Click += new System.EventHandler(this.Insertar_Click);
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Eliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Eliminar.Location = new System.Drawing.Point(158, 158);
+            this.Eliminar.Margin = new System.Windows.Forms.Padding(2);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(104, 38);
+            this.Eliminar.TabIndex = 1;
+            this.Eliminar.Text = "Eliminar Detras";
+            this.Eliminar.UseVisualStyleBackColor = false;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
+            // 
+            // Primero
+            // 
+            this.Primero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Primero.Location = new System.Drawing.Point(-1, 208);
+            this.Primero.Margin = new System.Windows.Forms.Padding(2);
+            this.Primero.Name = "Primero";
+            this.Primero.Size = new System.Drawing.Size(61, 31);
+            this.Primero.TabIndex = 2;
+            this.Primero.Text = "primero";
+            this.Primero.UseVisualStyleBackColor = true;
+            this.Primero.Click += new System.EventHandler(this.Primero_Click);
+            // 
+            // Anterior
+            // 
+            this.Anterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Anterior.Location = new System.Drawing.Point(81, 208);
+            this.Anterior.Margin = new System.Windows.Forms.Padding(2);
+            this.Anterior.Name = "Anterior";
+            this.Anterior.Size = new System.Drawing.Size(66, 31);
+            this.Anterior.TabIndex = 3;
+            this.Anterior.Text = "Anterior";
+            this.Anterior.UseVisualStyleBackColor = true;
+            this.Anterior.Click += new System.EventHandler(this.Anterior_Click);
+            // 
+            // Siguiente
+            // 
+            this.Siguiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Siguiente.Location = new System.Drawing.Point(183, 200);
+            this.Siguiente.Margin = new System.Windows.Forms.Padding(2);
+            this.Siguiente.Name = "Siguiente";
+            this.Siguiente.Size = new System.Drawing.Size(79, 32);
+            this.Siguiente.TabIndex = 4;
+            this.Siguiente.Text = "Siguiente";
+            this.Siguiente.UseVisualStyleBackColor = true;
+            this.Siguiente.Click += new System.EventHandler(this.Siguiente_Click);
+            // 
+            // Ultimo
+            // 
+            this.Ultimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ultimo.Location = new System.Drawing.Point(281, 208);
+            this.Ultimo.Margin = new System.Windows.Forms.Padding(2);
+            this.Ultimo.Name = "Ultimo";
+            this.Ultimo.Size = new System.Drawing.Size(75, 31);
+            this.Ultimo.TabIndex = 5;
+            this.Ultimo.Text = "Ultimo";
+            this.Ultimo.UseVisualStyleBackColor = true;
+            this.Ultimo.Click += new System.EventHandler(this.Ultimo_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(183, 88);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 21);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(183, 115);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 21);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Miriam", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label1.Location = new System.Drawing.Point(11, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nombre de Participante:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("MingLiU-ExtB", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(11, 113);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Numero :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.MintCream;
+            this.label3.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(10, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(397, 22);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Lista Doblemente Enlazada Circular";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 283);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(91, 95);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(137, 283);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(125, 108);
+            this.listBox2.TabIndex = 13;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(525, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "horas";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Ivory;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(420, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 15);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "mat. con mayor cant. de hrs";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(434, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "materia";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Ivory;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(431, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 15);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "promedio de horas";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(470, 219);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "horas";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Ivory;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label10.Location = new System.Drawing.Point(420, 265);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(168, 15);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "horas como numeros primos ";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(454, 283);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(104, 95);
+            this.listBox3.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Ivory;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(420, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(157, 15);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "mat. con mayor cant. de hrs";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(434, 85);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "materia";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(525, 86);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(33, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "horas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(622, 440);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Ultimo);
+            this.Controls.Add(this.Siguiente);
+            this.Controls.Add(this.Anterior);
+            this.Controls.Add(this.Primero);
+            this.Controls.Add(this.Eliminar);
+            this.Controls.Add(this.Insertar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Insertar;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.Button Primero;
+        private System.Windows.Forms.Button Anterior;
+        private System.Windows.Forms.Button Siguiente;
+        private System.Windows.Forms.Button Ultimo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_Doblemente_Enlazada_Circular_N_1
+{
+    public partial class Form1 : Form
+    {private int  num_menor=100;
+        public int cont = 0;
+        public int sum = 0;
+        public int nuvp = 0;
+
+        ListaAsig Lista1;
+        public Form1()
+        {
+            InitializeComponent();
+            Lista1 = new ListaAsig();
+        }
+
+        private void Insertar_Click(object sender, EventArgs e)
+        {
+            int p=int.Parse(textBox2.Text);
+            string x = textBox1.Text;
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Escriba los datos");
+                return;
+            }
+            else
+            {
+                Lista1.CrearLista(textBox1.Text, int.Parse(textBox2.Text));
+                textBox1.Text = "Escriba aqui";
+                textBox2.Text = "";
+                textBox1.Focus();
+                textBox1.Text = null;
+                textBox2.Text = null;
+                listBox1.Items.Add(x);
+                listBox2.Items.Add(p);
+
+            }
+
+            if (p > nuvp)
+            {
+                nuvp = p;
+                label4.Text = nuvp.ToString();
+                label6.Text = x.ToString();
+            }
+            if(p<num_menor)
+            {
+                num_menor = p;
+                label12.Text = num_menor.ToString();
+                label11.Text = x.ToString();
+            }
+
+            if (p != 0)
+            {
+                cont++;
+                sum = p + sum;
+
+            }
+            int prom = sum / cont;
+
+            label9.Text = prom.ToString();
+            int cant_div = 0;
+            for (int i = 1; i <= p; i++)
+            {
+                if (p % i == 0)
+                {
+                    cant_div++;
+                }
+
+            }
+            if (cant_div == 2)
+            {
+                listBox3.Items.Add(p);
+
+            }
+
+
+        }
+
+        private void Eliminar_Click(object sender, EventArgs e)
+        {
+            //TNodo elim;
+            if (Lista1.BuscarAsignado(textBox1.Text))
+            {
+                //elim = (TNodo)Lista1.Eliminar();
+                Lista1.EliminarLista(textBox1.Text);
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+            else
+            {
+             MessageBox.Show("Lista Vacia");
+            }
+            
+
+
+                
+        }
+
+        private void Primero_Click(object sender, EventArgs e)
+        {
+            TNodo nodoPrimero;
+            nodoPrimero = (TNodo)Lista1.GetPrimero();
+            if(nodoPrimero == null)
+            {
+                MessageBox.Show("Lista vacia");
+                return;
+            }
+            else
+            {
+                textBox1.Text = nodoPrimero.DameNombre();
+                textBox2.Text = nodoPrimero.DameNumero().ToString();
+            }
+        }
+
+        private void Anterior_Click(object sender, EventArgs e)
+        {
+            if (Lista1.BuscarAsignado(textBox1.Text) == true)
+            {
+                TNodo nodoAnterior = (TNodo)Lista1.GetAnteriorCursor();
+                textBox1.Text = nodoAnterior.DameNombre();
+                textBox2.Text = (nodoAnterior.DameNumero()).ToString();
+            }
+            else
+            {
+                MessageBox.Show("La lista no tiene sucesor");
+            }
+        }
+
+        private void Siguiente_Click(object sender, EventArgs e)
+        {
+            TNodo nodoSiguiente;
+            if (Lista1.BuscarAsignado(textBox1.Text) == true)
+            {
+                nodoSiguiente = (TNodo)Lista1.GetProxCursor();
+                textBox1.Text = nodoSiguiente.DameNombre();
+                textBox2.Text = (nodoSiguiente.DameNumero()).ToString();
+            }
+            else
+            {
+                MessageBox.Show("La lista no tiene nodo siguiente");
+            }
+        }
+
+        private void Ultimo_Click(object sender, EventArgs e)
+        {
+            TNodo nodoUltimo;
+            nodoUltimo = (TNodo)Lista1.GetUltimo();
+            if (nodoUltimo == null)
+            {
+                MessageBox.Show("Lista Vacia");
+                return;
+            }
+            else
+            {
+                textBox1.Text = nodoUltimo.DameNombre();
+                textBox2.Text = nodoUltimo.DameNumero().ToString();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+      
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
